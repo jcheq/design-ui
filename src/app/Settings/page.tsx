c8ecfda5aa50bdf5cb921432ac74ff77d345fda3
jcheq <cderic2019@gmail.com>
2024-07-28 12:40:09 -0700

    fixed bar

--- src/app/Settings/page.tsx
+++ src/app/Settings/page.tsx
@@ -2,25 +2,38 @@ import { Search, Plus } from "lucide-react";
 
 const Settings = () => {
     return (
-      <div className="settings">
-        <div className="userName">
-          <h1> Mia de Silva</h1>
-          {/* <p className="flex-direction">Manage your details and personal preferences here.</p> */}
-        </div>
-        <div className="userActions">
-          <div>
-            <Search/>
-            <input type="text" className="search" placeholder="Search"/>
+      <div className="layout">
+        <div className="settings">
+          <div className="user">
+            <h1> Mia de Silva</h1>
+            <p className="description">Manage your details and personal preferences here.</p>
           </div>
-    
-          <button className="invite">
-            <Plus className="size-5"/>
-            Invite
-          </button>
 
-          <button className="upgrade">Upgrade</button>
+          
+          <div className="userActions">
+            <div className="borderSearch">
+              <Search/>
+              <input type="text" className="search" placeholder="Search"/>
+            </div>
+      
+            <button className="invite">
+              <Plus className="size-4"/>
+              Invite
+            </button>
+
+            <button className="upgrade">Upgrade</button>
+          </div>
+
+
+          
         </div>
+        <div className="erf"> 
+          <h1> Mia de Silva</h1>
+          
+        </div>
+        
       </div>
+      
 
     ) 
   };
